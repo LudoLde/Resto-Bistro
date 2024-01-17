efd6e573109493236cf4be39419d3734fde5c451
Ludovic Labaude <ludzy@MBP-de-Ludovic.lan>
2024-01-17 14:45:35 +0100

    erreurs

--- Resto_bistrot/vite.config.js
+++ Resto_bistrot/vite.config.js
@@ -1,14 +1,26 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import reactSlideshowImage from "react-slideshow-image";
-import { fontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+
+// Configure Font Awesome
+library.add(fab, far, fas);
 
 // https://vitejs.dev/config/
 export default defineConfig({
    plugins: [react()],
    build: {
       rollupOptions: {
-         external: [reactSlideshowImage, "@fortawesome/react-fontawesome"],
+         external: [
+            "react-slideshow-image",
+            "@fortawesome/fontawesome-svg-core",
+            "@fortawesome/free-brands-svg-icons",
+            "@fortawesome/free-regular-svg-icons",
+            "@fortawesome/free-solid-svg-icons",
+         ],
       },
    },
 });
