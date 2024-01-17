80dfbf8fb51caf4745ebd0f5fb1856aae7fab5b5
Ludovic Labaude <ludzy@MBP-de-Ludovic.lan>
2024-01-17 14:34:47 +0100

    erreur

--- Resto_bistrot/vite.config.js
+++ Resto_bistrot/vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import reactSlideshowImage from "react-slideshow-image";
+import { fontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 // https://vitejs.dev/config/
 export default defineConfig({
    plugins: [react()],
    build: {
       rollupOptions: {
-         external: [reactSlideshowImage],
+         external: [reactSlideshowImage, "@fortawesome/react-fontawesome"],
       },
    },
 });
